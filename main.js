3d7925db50e8dd61910c7ef26ca0b66106a90ee1
abdullahahtesham <146721363+abdullahahtesham@users.noreply.github.com>
2024-05-07 19:28:29 +0500

    add this

---
+++ main.js
@@ -0,0 +1,6 @@
+import { name01 } from "./modules.js";
+console.log(name01);
+import fullName from "./modules.js"; //with the default export
+console.log(fullName());
+import fullName1 from "./modules/namemodules.js";
+console.log(fullName1());
---
+++ modules.js
@@ -0,0 +1,9 @@
+export const name01 = "Abdullah-Ahtesham";
+export default function fullName() {
+    const name1 = "Abdullah";
+    const name2 = "Ahtesham";
+    return name1 + " " + name2;
+}
+//default export is use only one time in file  and it can be imported only once and import by same name
+console.log(fullName());
+
