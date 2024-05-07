b9fc414fabb1b02220b2b9e67bd271a0d80f6941
abdullahahtesham <146721363+abdullahahtesham@users.noreply.github.com>
2024-05-07 19:27:27 +0500

    add this

---
+++ modules/namemodules.js
@@ -0,0 +1,8 @@
+export const name01 = "Abdullah-Ahtesham";
+export default function fullName1() {
+    const name1 = "Abdullah";
+    const name2 = "Ahtesham";
+    return name1 + " " + name2;
+}
+//default export is use only one time in file
+console.log(fullName1());
---
+++ modules/namemodules.ts
@@ -0,0 +1,10 @@
+export const name01: string = "Abdullah-Ahtesham";
+export default function fullName1() {
+  const name1: string = "Abdullah";
+  const name2: string = "Ahtesham";
+
+  return name1 + " " + name2;
+}
+//default export is use only one time in file
+
+console.log(fullName1());
